2581f96778233cd36cbe70438b086b8a58976c61
Jakub Zakrzewski <jzakrzewski.jz@gmail.com>
2025-12-23 12:19:16 +0100

    [Frontend] Support using chat template as custom score template for reranking models (#30550)
    Signed-off-by: Jakub Zakrzewski <jzakrzewski@nvidia.com>
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>
    Signed-off-by: wang.yuqi <noooop@126.com>
    Co-authored-by: wang.yuqi <yuqi.wang@daocloud.io>
    (cherry picked from commit 23daef548dd1b33ba6ecb00c8c65e69f17102d13)

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -202,6 +202,7 @@ _EMBEDDING_MODELS = {
     "GteNewModel": ("bert_with_rope", "GteNewModel"),
     "InternLM2ForRewardModel": ("internlm2", "InternLM2ForRewardModel"),
     "JambaForSequenceClassification": ("jamba", "JambaForSequenceClassification"),  # noqa: E501
+    "LlamaBidirectionalModel": ("llama", "LlamaBidirectionalModel"),
     "LlamaModel": ("llama", "LlamaForCausalLM"),
     **{
         # Multiple models share the same architecture, so we include them all
@@ -245,6 +246,11 @@ _CROSS_ENCODER_MODELS = {
         "bert_with_rope",
         "GteNewForSequenceClassification",
     ),
+    "JinaVLForRanking": ("jina_vl", "JinaVLForSequenceClassification"),
+    "LlamaBidirectionalForSequenceClassification": (
+        "llama",
+        "LlamaBidirectionalForSequenceClassification",
+    ),
     "ModernBertForSequenceClassification": (
         "modernbert",
         "ModernBertForSequenceClassification",
@@ -258,8 +264,6 @@ _CROSS_ENCODER_MODELS = {
         "roberta",
         "RobertaForSequenceClassification",
     ),
-    # [Auto-converted (see adapters.py)]
-    "JinaVLForRanking": ("jina_vl", "JinaVLForSequenceClassification"),  # noqa: E501,
 }
 
 _MULTIMODAL_MODELS = {
